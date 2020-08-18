ad29d238c16551e44c06feb4a98dd918af2d9a50
kimbochen <chentenghung@gmail.com>
2020-08-18 13:09:33 +0800

    Used new scheduler and fixed checkpoint logic.

--- train.py
+++ train.py
@@ -4,48 +4,51 @@ from statistics import mean
 import torch
 import torch.nn as nn
 from torch.optim import Adam
-from torch.optim.lr_scheduler import ExponentialLR
+from torch.optim.lr_scheduler import OneCycleLR
 from torch.utils.data import DataLoader
 from tqdm import tqdm
 
-from chamfer_distance import ChamferLoss
 from dataset import PPFDataset
+from loss import ChamferLoss
 from model import AutoEncoder
 from configs import *
 
 
 def train(model, loss_func, optimizer, scheduler, data_loaders, args):
     train_dl, val_dl = data_loaders
-    loss = 0.0
+    min_loss = 309.805
 
     for epoch in range(args.num_epochs):
         print('Training ...')
         model.train()
-        for x_batch, y_batch in tqdm(train_dl):
-            batch_forward(model, loss_func, x_batch, y_batch)
-            optimizer.step()
-            optimizer.zero_grad()
+        train_loss = [
+            batch_forward(model, loss_func, x_batch, y_batch, optimizer, scheduler)
+            for x_batch, y_batch in tqdm(train_dl)
+        ]
+
+        # if (epoch + 1) % args.scheduler_interval == 0:
+        #     scheduler.step()
 
         print('Evaluating ...')
         model.eval()
         with torch.no_grad():
-            loss_hist = [
+            val_loss = [
                 batch_forward(model, loss_func, x_batch, y_batch)
                 for x_batch, y_batch in tqdm(val_dl)
             ]
 
-        if (epoch + 1) % args.scheduler_interval == 0:
-            scheduler.step()
-
-        if mean(loss_hist) < loss and (epoch + 1) > args.checkpoint_epoch:
+        if (epoch + 1) > args.checkpoint_epoch and mean(val_loss) < min_loss:
             print('Saving checkpoint ...')
-            torch.save(model.state_dict(), args.checkpoint_path)
+            torch.save(model.encoder.state_dict(), args.checkpoint_path)
+            min_loss = mean(val_loss)
 
-        loss = mean(loss_hist)
-        print(f'Epoch {epoch:03d} validation loss: {loss:.4f}\n')
+        log = 'Epoch {:03d} | Train loss: {:.4f} | Validation loss: {:.4f}\n'.format(
+            epoch, mean(train_loss), mean(val_loss)
+        )
+        print(log)
 
 
-def batch_forward(model, loss_func, x_batch, y_batch):
+def batch_forward(model, loss_func, x_batch, y_batch, optimizer=None, scheduler=None):
     x_batch, y_batch = x_batch.to(DEVICE), y_batch.to(DEVICE)
 
     pred = model(x_batch)
@@ -53,14 +56,17 @@ def batch_forward(model, loss_func, x_batch, y_batch):
 
     if model.training:
         loss.backward()
-    else:
-        return loss.item()
+        optimizer.step()
+        optimizer.zero_grad()
+        scheduler.step()
+
+    return loss.item()
 
 
 def init_weights(m):
     if isinstance(m, nn.Linear):
         nn.init.kaiming_normal_(m.weight.data, nonlinearity='relu')
-        nn.init.constant_(m.bias.data, 0)
+        nn.init.constant_(m.bias.data, 0.0)
 
 
 if __name__ == '__main__':
@@ -71,26 +77,30 @@ if __name__ == '__main__':
 
     print(f'Using device {DEVICE}')
 
+    # Data loaders
+    train_ds = PPFDataset(**TRAIN_DS_ARGS)
+    train_dl = DataLoader(train_ds, **TRAIN_DL_ARGS)
+
+    val_ds = PPFDataset(**VAL_DS_ARGS)
+    val_dl = DataLoader(val_ds, **VAL_DL_ARGS)
+
+    print('Training set: {}. Validation set: {}.\n'.format(
+        train_ds.__len__(), val_ds.__len__()
+    ))
+
     # Model
     model = AutoEncoder(NUM_PTS_PER_PATCH)
     model.apply(init_weights).to(DEVICE)
 
     loss_func = ChamferLoss()
     optimizer = Adam(model.parameters(), **OPTIMIZER_ARGS)
-    scheduler = ExponentialLR(optimizer, SCHEDULER_GAMMA)
+    scheduler = OneCycleLR(
+        optimizer, max_lr=1e-2,
+        steps_per_epoch=len(train_dl),
+        epochs = TRAINER_ARGS.num_epochs
+    )
 
-    # Data loaders
-    train_ds = PPFDataset(**TRAIN_DS_ARGS)
-    train_dl = DataLoader(train_ds, **DATALOADER_ARGS)
-
-    val_ds = PPFDataset(**TEST_DS_ARGS)
-    val_dl = DataLoader(val_ds, **DATALOADER_ARGS)
-
-    print('Training set: {}, Validation set: {}.\n'.format(
-        train_ds.__len__(), val_ds.__len__()
-    ))
-
-    Path(TRAIN_ARGS.checkpoint_path).parent.mkdir(parents=True, exist_ok=True)
+    Path(TRAINER_ARGS.checkpoint_path).parent.mkdir(parents=True, exist_ok=True)
 
     # Training
-    train(model, loss_func, optimizer, scheduler, (train_dl, val_dl), TRAIN_ARGS)
+    train(model, loss_func, optimizer, scheduler, (train_dl, val_dl), TRAINER_ARGS)
